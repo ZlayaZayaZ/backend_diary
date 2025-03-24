eeaf67167d6c7d3e33f5bfba9f3cc0566d9947fc
Vasilkova Lyubov <damirana_@mail.ru>
2025-03-24 12:03:35 +0700

    setting up nested links

--- diary_back/urls.py
+++ diary_back/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('diary/', include('emotional_diary.urls')),
+    path('habit/', include('habit_tracker.urls')),
     path('admin/', admin.site.urls),
 ]
